60b7d0effb444763d6d61a89bf5501ad3c9fc086
chris0610 <chrislow360@gmail.com>
2022-06-28 16:35:40 +0800

    wrap _app.tsx with layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 
+import Layout from "@components/Layout";
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default appWithTranslation(App);
