043b3741cc00f15ea664d66a9e12b0913d6f699a
chris0610 <chrislow360@gmail.com>
2022-06-28 16:34:25 +0800

    add footer component

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import Container from "@components/Container";
+
+const Footer = () => {
+  return (
+    <Container background="bg-washed py-12">
+      <div className="flex w-full justify-between text-sm">
+        <div className="flex gap-4">
+          {/* LOGO */}
+          <div className="mt-1 w-12">
+            <Image src="/static/images/logo.png" width={48} height={36} />
+          </div>
+          <div>
+            <div className="mb-2">
+              <p className="text-base font-bold">PRIME MINISTER'S DEPARTMENT</p>
+              <p className="text-base font-bold">
+                DEPARTMENT OF STATISTICS MALAYSIA
+              </p>
+            </div>
+            {/* COPYRIGHT */}
+            <p className="text-dim">© 2022 Department of Statistics Malaysia</p>
+          </div>
+        </div>
+        <div className="flex gap-14">
+          {/* SOURCES */}
+          <div className="flex flex-col gap-2">
+            <p className="font-bold">Source</p>
+            <p className="text-dim">
+              Website:{" "}
+              <Link href="/">
+                <span className="text-footer-link">AKSARA</span>
+              </Link>
+            </p>
+            <p className="text-dim">
+              Data:{" "}
+              <Link href="/">
+                <span className="text-footer-link">DOSM Github</span>
+              </Link>
+            </p>
+          </div>
+          {/* RESOURCES */}
+          <div className="flex flex-col gap-2">
+            <p className="font-bold">Resources</p>
+            <Link href="/">
+              <span className="text-footer-link">Meet the team</span>
+            </Link>
+            <Link href="/">
+              <span className="text-footer-link">DOSM portal</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Footer;
