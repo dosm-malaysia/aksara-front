2bd2b279be11c2d418962c6868773bdaf8eedb5d
chris0610 <chrislow360@gmail.com>
2022-06-29 16:59:15 +0800

    create hero component

---
+++ components/Hero/index.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from "react";
+
+import Container from "@components/Container";
+
+type HeroProps = {
+  background?: string;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const Hero: FunctionComponent<HeroProps> = ({
+  background,
+  className,
+  children,
+}) => {
+  return (
+    <Container background={background} className={`py-12 ${className}`}>
+      {children}
+    </Container>
+  );
+};
+
+export default Hero;
