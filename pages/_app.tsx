2d7ecf9eac8954f9bc780d5dc40586aeb7e31fa7
irfanismail <irfan.ismail96@gmail.com>
2022-07-04 11:03:09 +0800

    fix: default layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import { AppPropsLayout } from "@lib/types";
+import { Layout } from "@components/index";
 
 function App({ Component, pageProps }: AppPropsLayout) {
-    const layout = Component.layout ?? ((page: any) => page);
-    return layout(<Component {...pageProps} />);
+  const layout = (page: any) => (Component.layout ? page : <Layout>{page}</Layout>);
+  return layout(<Component {...pageProps} />);
 }
 
 export default appWithTranslation(App);
