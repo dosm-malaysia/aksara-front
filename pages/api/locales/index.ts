f1644a1ed991b60e6dbcf0bcd93aea1895104656
irfanismail <irfan.ismail96@gmail.com>
2022-07-06 07:40:51 +0800

    wip: localeupdate

--- pages/api/locales/index.ts
+++ pages/api/locales/index.ts
@@ -25,45 +25,49 @@ const middleware = (request: NextRequestMiddleware, response: NextApiResponse) =
 };
 
 const handler = async (request: NextRequestMiddleware, response: NextApiResponse) => {
-  const ok = middleware(request, response);
+  //   const ok = middleware(request, response);
 
-  if (ok) {
-    console.log("Initialize locale update...");
-    const manifestFile = resolve(process.cwd(), "public/locales/manifest.json");
+  //   console.log(request.body.payload.translations.update);
 
-    // Fetch the locales from backend API
-    const locales = await get("CMS", "items/locales?fields=*");
-    console.log("Locales fetched...");
+  //   if (ok) {
+  //     console.log("Initialize locale update...");
+  const manifestFile = resolve(process.cwd(), "public/locales/manifest.json");
 
-    // Validate locale manifest, return the locales that needs updating
-    let updatedLocales: Array<any> = await validateManifest(manifestFile, locales);
-    console.log("Locales validated. Required to update: ", updatedLocales.length);
+  // Fetch the locales from backend API
+  const locales = await get("CMS", "items/locales?fields=*.*");
+  console.log("Locales fetched...");
+  await generateManifestFile(locales, manifestFile);
 
-    // Generate locale files as required (based on updatedLocales[]) & regenerate the manifest file.
-    if (updatedLocales.length > 0) {
-      updatedLocales.forEach(locale => {
-        const localeDir = resolve(process.cwd(), `${LOCALE_DIR}/${locale.language}`);
-        const localeFile = resolve(process.cwd(), `${LOCALE_DIR}/${locale.language}/common.json`);
+  // Validate locale manifest, return the locales that needs updating
+  let updatedLocales: Array<any> = await validateManifest(manifestFile, locales);
+  console.log("Locales validated. Required to update: ", updatedLocales.length);
 
-        access(localeDir, async err => {
-          if (!err) {
-            // If exists, update the locale files
+  // Generate locale files as required (based on updatedLocales[]) & regenerate the manifest file.
+  // TODO: Fix this part
+  if (updatedLocales.length > 0) {
+    updatedLocales.forEach(locale => {
+      const localeDir = resolve(process.cwd(), `${LOCALE_DIR}/${locale.languages_code}`);
+      const localeFile = resolve(process.cwd(), `${LOCALE_DIR}/${locale.language}/common.json`);
+
+      access(localeDir, async err => {
+        if (!err) {
+          // If exists, update the locale files
+          await createLocaleFile(localeFile, locale.json, locale.language);
+        } else {
+          // Else, create directory & the locale files afterwards
+          mkdir(localeDir, async err => {
+            if (err) throw err;
             await createLocaleFile(localeFile, locale.json, locale.language);
-          } else {
-            // Else, create directory & the locale files afterwards
-            mkdir(localeDir, async err => {
-              if (err) throw err;
-              await createLocaleFile(localeFile, locale.json, locale.language);
-            });
-          }
-        });
+          });
+        }
       });
-      await generateManifestFile(locales, manifestFile);
-    } else {
-      console.info("All locales are up-to-date. Proceeding with the build... 🚀");
-    }
-    response.status(200).json({ message: "Request successful" });
+    });
+    await generateManifestFile(locales, manifestFile);
+  } else {
+    console.info("All locales are up-to-date. Proceeding with the build... 🚀");
   }
+  response.status(200).json({ message: "Request successful" });
+
   response.status(400).json({ error: "Bad request" });
 };
 
@@ -74,25 +78,35 @@ const validateManifest = (manifestPath: string, locales: any): Promise<Array<any
 
       if (!err) {
         /**
-                    If exists, read the existing manifest file & compare with the API result.
-                    
-                    Comparison condition:
-                    1. Compare if have same keys. If new key, add to updatedLocale (means new language added)
-                    2. Compare the version timestamp. If different, add to updatedLocale (means there is a new update to the locale)
-                    3. Else, do nothing.
-                 */
+        If exists, read the existing manifest file & compare with the API result.
+        
+        Comparison condition:
+        1. Compare if have same keys. If new key, add to updatedLocale (means new language added)
+        2. Compare the version timestamp. If different, add to updatedLocale (means there is a new update to the locale)
+        3. Else, do nothing.
+     */
         const { versions } = JSON.parse(readManifestFile(manifestPath));
 
         locales.forEach((locale: any) => {
-          if (!Object.keys(versions).includes(locale.language)) updatedLocales.push(locale);
-          else if (new Date(locale.date_updated).getTime() !== versions[locale.language])
-            updatedLocales.push(locale);
+          if (!Object.keys(versions).includes(locale.path)) {
+            updatedLocales = [...updatedLocales, ...locale.translations];
+          } else if (locale.translations.length > 0) {
+            locale.translations.forEach((item: any) => {
+              if (
+                versions[locale.path][item.languages_code] !== new Date(item.timestamp).getTime()
+              ) {
+                updatedLocales.push(locale);
+              }
+            });
+          }
         });
 
         resolve(updatedLocales);
       } else {
         // Else, update all locale files
-        updatedLocales = [...locales];
+        updatedLocales = locales.reduce((previous: any, current: any) => {
+          return [...previous, ...current.translations];
+        }, []);
         resolve(updatedLocales);
       }
     });
@@ -104,12 +118,17 @@ const readManifestFile = (path: string) => {
 };
 
 const generateManifestFile = async (locales: any, manifestPath: string) => {
-  const versions = locales.reduce((previous: any, current: any) => {
-    return {
-      ...previous,
-      [current.language]: new Date(current.date_updated).getTime(),
-    };
-  }, {});
+  let versions: Record<string, Record<string, number>> = {};
+
+  locales.forEach((locale: any) => {
+    versions[locale.path] = locale.translations.reduce((previous: any, current: any) => {
+      return {
+        ...previous,
+        [current.languages_code]: new Date(current.timestamp).getTime(),
+      };
+    }, {});
+  });
+
   const manifest = {
     name: MANIFEST_NAME,
     versions: versions,
