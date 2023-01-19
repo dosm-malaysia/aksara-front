d06f57eab1c74b7e7d151bc0ce240e0d1f4fda33
irfanismail <irfan.ismail96@gmail.com>
2023-01-19 15:29:19 +0800

    hotfix: interest rate update

--- dashboards/interest-rates/index.tsx
+++ dashboards/interest-rates/index.tsx
@@ -178,7 +178,7 @@ const InterestRatesDashboard: FunctionComponent<InterestRatesDashboardProps> = (
         <Section
           title={t("interest_rates.section_1.title")}
           description={t("interest_rates.section_1.description")}
-          date={timeseries.data_as_of}
+          date={timeseries_opr.data_as_of}
         >
           <div className="space-y-8">
             <div className="grid grid-cols-1 gap-4 lg:flex lg:flex-row">
